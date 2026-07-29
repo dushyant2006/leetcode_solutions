cf2bef38d4e06e9d515c2287a456d3a3468f574f
Dushyant Sharma <dushyant.sharma_cs.aiml24@gla.ac.in>
2026-07-29 09:36:42 +0530

    Time: 18 ms (74.19%) | Memory: 47.3 MB (41.94%) - LeetSync

--- 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.java
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.java
@@ -1,99 +1,73 @@
-import java.util.*;
-
 class Solution {
-    private static final long LIMIT = 1_000_001L;
-
     public String smallestPalindrome(String s, int k) {
         int[] freq = new int[26];
-
-        for (char c : s.toCharArray())
+        for (char c : s.toCharArray()) {
             freq[c - 'a']++;
-
+        }
+        
         int[] half = new int[26];
         StringBuilder mid = new StringBuilder();
-
         int m = 0;
-
-        for (int i = 0; i < 26; i++) {
+        
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] % 2 != 0) {
+                mid.append((char) (i + 'a'));
+            }
             half[i] = freq[i] / 2;
             m += half[i];
-
-            if ((freq[i] & 1) == 1)
-                mid.append((char) ('a' + i));
         }
-
-        long[][] C = buildComb(m);
-
-        long total = countWays(half, m, C);
-
-        if (total < k)
+        
+        if (getWays(half, k) < k) {
             return "";
-
-        StringBuilder left = new StringBuilder();
-
-        while (m > 0) {
-
-            for (int c = 0; c < 26; c++) {
-
-                if (half[c] == 0)
-                    continue;
-
-                half[c]--;
-
-                long ways = countWays(half, m - 1, C);
-
-                if (ways >= k) {
-                    left.append((char) ('a' + c));
-                    m--;
-                    break;
-                } else {
-                    k -= ways;
-                    half[c]++;
+        }
+        
+        StringBuilder firstHalf = new StringBuilder();
+        for (int i = 0; i < m; ++i) {
+            for (int c = 0; c < 26; ++c) {
+                if (half[c] > 0) {
+                    half[c]--;
+                    long ways = getWays(half, k);
+                    
+                    if (ways >= k) {
+                        firstHalf.append((char) (c + 'a'));
+                        break;
+                    } else {
+                        k -= ways;
+                        half[c]++;
+                    }
                 }
             }
         }
-
-        StringBuilder right = new StringBuilder(left).reverse();
-
-        return left.toString() + mid + right.toString();
+        
+        StringBuilder res = new StringBuilder(firstHalf);
+        res.append(mid);
+        res.append(firstHalf.reverse());
+        return res.toString();
     }
-
-    private long[][] buildComb(int n) {
-
-        long[][] C = new long[n + 1][n + 1];
-
-        for (int i = 0; i <= n; i++) {
-
-            C[i][0] = C[i][i] = 1;
-
-            for (int j = 1; j < i; j++) {
-
-                C[i][j] = Math.min(LIMIT, C[i - 1][j - 1] + C[i - 1][j]);
+    
+    private long getWays(int[] f, long targetK) {
+        long ways = 1;
+        int currLen = 0;
+        for (int count : f) {
+            if (count > 0) {
+                currLen += count;
+                long n = currLen;
+                long r = count;
+                
+                if (r > n - r) r = n - r;
+                long curNCr = 1;
+                
+                for (int i = 1; i <= r; ++i) {
+                    curNCr = curNCr * (n - i + 1) / i;
+                    if (curNCr > targetK) {
+                        curNCr = targetK + 1;
+                        break;
+                    }
+                }
+                ways *= curNCr;
+                if (ways > targetK) return targetK + 1;
             }
         }
-
-        return C;
-    }
-
-    private long countWays(int[] cnt, int total, long[][] C) {
-
-        long ans = 1;
-
-        int rem = total;
-
-        for (int x : cnt) {
-
-            if (x == 0)
-                continue;
-
-            ans *= C[rem][x];
-
-            if (ans > LIMIT)
-                ans = LIMIT;
-
-            rem -= x;
-        }
-
-        return ans;
+        return ways;
     }
 }
