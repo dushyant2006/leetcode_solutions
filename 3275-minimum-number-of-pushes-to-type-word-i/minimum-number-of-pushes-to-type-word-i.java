7bfe486e2295e21dc99dc9aa4a403cd9f1fb0e0b
Dushyant Sharma <dushyant.sharma_cs.aiml24@gla.ac.in>
2026-07-30 20:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (28.23%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0;
 
         for (int i = 0; i < word.length(); i++) {
-            ans += (i / 8) + 1;
+            ans += (i / 8) +1;
         }
 
         return ans;
