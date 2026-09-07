506e51dd2d5db676f318c12574eda8fb9f64d5e3
Dushyant Sharma <dushyant.sharma_cs.aiml24@gla.ac.in>
2026-09-07 10:17:19 +0530

    Time: 1550 ms (10.62%) | Memory: 48.2 MB (96.56%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int tar) {
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+                int sum=0;
+                for(int j=i;j<n;j++){
+                    sum+=nums[j];
+                    if(sum==tar){
+                    count++;
+                }
+                }
+                
+        }
+        return count;
+    }
+}
