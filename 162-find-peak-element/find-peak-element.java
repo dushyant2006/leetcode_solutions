324dbec9c28a624e263d6bdd88ba9322d36558b7
Dushyant Sharma <dushyant.sharma_cs.aiml24@gla.ac.in>
2026-09-14 10:28:56 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (75.28%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -6,10 +6,16 @@ class Solution {
         while (left < right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] > nums[mid + 1]) {
-                right = mid;
-            } else {
+            if (mid > 0 && mid < nums.length - 1 &&
+                nums[mid] > nums[mid - 1] &&
+                nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+
+            if (nums[mid] < nums[mid + 1]) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
