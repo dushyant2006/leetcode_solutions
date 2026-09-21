2bfb0f73daa50050bf358716c502026e82600f33
Dushyant Sharma <dushyant.sharma_cs.aiml24@gla.ac.in>
2026-09-21 10:47:08 +0530

    Time: 1 ms (99.57%) | Memory: 42.4 MB (78.76%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,9 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        long r=x;
-        while(r*r>x){
-            r=(r+x/r)/2;
+        if (x == 0) {
+            return 0;
         }
-        return (int)r;
+        int first = 1, last = x;
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return last;
     }
 }
